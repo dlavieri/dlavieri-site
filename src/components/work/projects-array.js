d36c0fa9d9b07068a2da7d8e3bfa419ce757dd8c
david lavieri <dvlavieri@gmail.com>
2019-05-29 12:55:33 -0700

    new project

--- src/components/work/projects-array.js
+++ src/components/work/projects-array.js
@@ -1,19 +1,27 @@
 import pokeshop from '../../PokeShop.png';
 import todo from '../../ToDoList.png';
+import weather from '../../WeatherApp.png';
 
 export const projects = [
   {
     name: 'Pokemon e-Commerce',
     desc: 'Full e-commerce front-end, will search-by-name, filter-by-attribute, more info modals, and checkout with credit-card and email validation.',
-    tags: ['React.js', 'CSS', 'eCommerce', 'Search', 'RegEx'],
+    tags: ['React.js', 'CSS', 'eCommerce', 'RegEx'],
     photo: pokeshop,
     link: 'https://thirsty-lumiere-58eac2.netlify.com/',
   },
   {
     name: 'To-Do-List',
-    desc: 'A simpled to-do-list app, where users can add tasks, mark them as completed, and see their progress toward completion.',
+    desc: 'A simple to-do-list app, where users can add tasks, mark them as completed, and see their progress toward completion.',
     tags: ['React.js', 'CSS', 'App'],
     photo: todo,
     link: 'https://brave-panini-417780.netlify.com/',
+  },
+  {
+    name: 'Weather App',
+    desc: 'A real-time weather App, using OpenWeatherMap API',
+    tags: ['React.js', 'API', 'App'],
+    photo: weather,
+    link: 'hopeful-johnson-739e3e.netlify.com',
   }
 ];
